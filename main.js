b0bb8df981ead8e5e42b9beeb6c40dc569ec5ec8
Goakem <195132463+Goakem@users.noreply.github.com.>
2026-06-28 07:49:56 -0400

    added the loop code
    There is a flaw in my code as it curretly returns the numbers and not the values in the array

--- main.js
+++ main.js
@@ -17,5 +17,15 @@ refused.textContent = "Refuse: ";
 
 // loop starts here
 
+//let newArray = 
+for (let i = 0; i < people.length; i++) {
+    if ((i === 'Phil') || (i === 'Lola')) {
+        refused.textContent += `${i}`;
+    } else if (i === people.length - 1) {
+        admitted.textContent += `${i}.`;
+    } else {
+        admitted.textContent += `${i},`
+    }
+}
 // refused.textContent += ...;
 // admitted.textContent += ...;
